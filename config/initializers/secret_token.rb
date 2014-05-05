69b72f562399e19238e13e0da7c3e4ab3303c4d0
Raphael Libert <raphael69libert@gmail.com>
2014-05-05 17:46:05 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lbcf::Application.config.secret_token = '59b21d1ea699e816bfbf2a352c39af0e4ae95a6df5815b8cf5d9d6a04eb9f7b01a562b287dca9fea91ced07ea0f7534f305e78572c745018f1487c17a70924dd'
